26c61fa746f50441b56c068d653f214c2869b200
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-10-02 22:01:20 +0000

    finished eldest and youngest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,6 +2,19 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list"})
   end
+
+  def youngest
+    @youngest_director = Director.order(dob: :desc).first 
+    render({ :template => "director_templates/youngest"})
+  end
+
+  def eldest
+    @eldest = Director.all.where.not({:dob => nil}).order(dob: :asc)
+    @old = @eldest[0]
+    
+    render({ :template => "director_templates/eldest"})
+  end
+
   
   def show
     the_id = params.fetch("the_id")
